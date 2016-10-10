bd060d1de169c892e7923ac62fbae3eac3b92229
Ian Kent <ThankMrSkeletal2016@gmail.com>
2016-10-10 15:42:23 -0500

    Initial Workspace Commit
    Copy into folder and use as workspace in andorid studio

---
+++ Zombie/app/src/main/java/downwardspiral/zombie/models/GeolocationOutputDoc.java
@@ -0,0 +1,28 @@
+package downwardspiral.zombie.models;
+
+/**
+ * Created by Ian on 10/8/2016.
+ */
+
+public class GeolocationOutputDoc extends Object {
+
+    int longitude;
+
+    int latitude;
+
+    public int getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(int longitude) {
+        this.longitude = longitude;
+    }
+
+    public int getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(int latitude) {
+        this.latitude = latitude;
+    }
+}
