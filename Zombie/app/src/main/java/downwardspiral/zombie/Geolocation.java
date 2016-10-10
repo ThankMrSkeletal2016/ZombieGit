8e06a37d70448fdde413263ccb21e57eaab40630
Ian Kent <ThankMrSkeletal2016@gmail.com>
2016-10-10 18:02:06 -0500

    New Prototype With GPS

---
+++ Zombie/app/src/main/java/downwardspiral/zombie/Geolocation.java
@@ -0,0 +1,28 @@
+package downwardspiral.zombie;
+
+/**
+ * Created by Ian on 10/10/2016.
+ */
+
+public class Geolocation {
+
+    private int gpsLongitude;
+
+    private int gpsLatitude;
+
+    public int getGpsLongitude() {
+        return gpsLongitude;
+    }
+
+    public void setGpsLongitude(int gpsLongitude) {
+        this.gpsLongitude = gpsLongitude;
+    }
+
+    public int getGpsLatitude() {
+        return gpsLatitude;
+    }
+
+    public void setGpsLatitude(int gpsLatitude) {
+        this.gpsLatitude = gpsLatitude;
+    }
+}
